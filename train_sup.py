83f6a9317bef44907d8f69941783fef1703cf20b
sailist <sailist@outlook.com>
2022-02-14 16:37:22 +0800

    run script

---
+++ train_sup.py
@@ -0,0 +1,18 @@
+import importlib
+from pprint import pprint
+from trainer.suptrainer import SupParams
+import trainer
+from pkgutil import iter_modules
+
+methods = {i.name for i in list(iter_modules(trainer.__path__))}
+
+if __name__ == '__main__':
+    params = SupParams()
+    params.module = None
+    params.from_args()
+    if params.module is None or params.module not in methods:
+        print('--module=')
+        pprint(methods)
+        exit(1)
+    module = importlib.import_module(f'track_bsl.{params.module}')
+    module.main()
