cfd43ec8773fe1afb5464edc0f762248b75d1b60
sailist <sailist@outlook.com>
2022-02-14 16:39:32 +0800

    i

--- train_sup.py
+++ train_sup.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
         print('--module=')
         pprint(methods)
         exit(1)
-    module = importlib.import_module(f'track_bsl.{params.module}')
+    module = importlib.import_module(f'trainer.{params.module}')
     module.main()
